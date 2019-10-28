39aada53300ab4f0ec0b980ef187cfa7c5bf8698
Sullivan Kromosoeto <sullivan.kromosoeto@outlook.com>
2019-10-28 09:11:47 +0100

    added call examples to apex.lang and apex.message

--- cheatsheet.js
+++ cheatsheet.js
@@ -1,26 +1,14 @@
-function(config) {
-    var cfg = cfg;
-   cfg.stretchColumns = false;
-   return config;
- }]
+$('#report_vkp_prices_report').find('tr.selected')[1] //Johannes says its the second element!!
+// add classes to a row report and then 
+// data-aos_type=#AOS_TYPE# data-tal_code=#TAL_CODE# data-omschrijving=#OMSCHRIJVING# data-rowno=#ROWNO#
+// data-formaat_ean=#TYPE# data-ean=#BARCODE# ta-rowno=#ROWNO#
+// data-lnd_code=#COUNTRY# data-btwcodeid=#VAT# data-rowno=#ROWNO#
+$('.datarow').data('fld_lnd_code')
+$('.datarow').data('prl_id')
  
- $('#report_vkp_prices_report').find('tr.selected')[1]
- 
- $('.datarow').data('fld_lnd_code')
- 
- $('#report_vkp_prices_report').find('tr.selected')[1]
- 
- $('.datarow').data('prl_id')
- 
- 
- javascript: doSubmit({   request:"DELETE",   set:{"P1_DEPTNO":10, "P1_EMPNO":5433});
- 
- data-aos_type=#AOS_TYPE# data-tal_code=#TAL_CODE# data-omschrijving=#OMSCHRIJVING# data-rowno=#ROWNO#
- 
- data-formaat_ean=#TYPE# data-ean=#BARCODE# ta-rowno=#ROWNO#
- 
- data-lnd_code=#COUNTRY# data-btwcodeid=#VAT# data-rowno=#ROWNO#
- 
+//submit via javascript
+javascript: doSubmit({   request:"DELETE",   set:{"P1_DEPTNO":10, "P1_EMPNO":5433});
+                       
 // Make use of datarow class to set item values
 $s("P7010_VKP_SELECTED_PRL_ID",$('#report_vkp_prices_report').find('td[headers="BUSINESS_UNIT"]:first').find("span").data('prl_id'));
 $s("P7010_VKP_SELECTED_LND_CODE",$('#report_vkp_prices_report').find('td[headers="BUSINESS_UNIT"]:first').find("span").data('fld_lnd_code'));
@@ -34,15 +22,13 @@ $('#selling_price_report h2').text("Retail Prices of " + $(this.triggeringElemen
 
                        
 // On click mark selected: jQuery selector: #report_hee_report > > > > tbody > > td
-
- //$s("P7010_HEE_SELECTED", $(this.triggeringElement).parent().children("td:first").text());
+$s("P7010_HEE_SELECTED", $(this.triggeringElement).parent().children("td:first").text());
 $s("P7010_HEE_SELECTED", $(this.triggeringElement).parent().children("td:nth-child(2)").text());
 
 // Mark selected parent row
 $('#hee_report').find('tr.selected').removeClass('selected');
 $(this.triggeringElement).closest('tr').addClass('selected');
 
-//------------
 // --> after refresh of report
 // Set selected
 $s("P7010_HEE_SELECTED", $(this.triggeringElement).find("td:first").text());
@@ -63,8 +49,22 @@ $s("P7010_VKP_SELECTED_LND_CODE",$('#report_vkp_prices_report').find('td[headers
 $('#report_vkp_prices_report').find('tr.selected').removeClass('selected');
 $('#report_vkp_prices_report').find('td[headers="BUSINESS_UNIT"]:first').closest("tr").addClass('selected');
 
-/* Old, working code to mark fisrt row 
-   Report static id = vkp_prices_report
-*/
+/* Old, working code to mark first row 
+   Report static id = vkp_prices_report */
 //$('#report_vkp_prices_report').find('tr.selected').removeClass('selected');
 //$('#report_vkp_prices_report').find('tbody tr:first-of-type').addClass('selected');
+
+//return a translateable text message
+apex.lang.getMessage( "SOME_KIND_OF_MESSAGE");
+
+//show errormessage
+apex.message.showErrors([
+    {
+        type:       "error",
+        location:   "page",
+        message:    "Page error has occurred!",
+        unsafe:     false
+    }
+]);
+
+                       
